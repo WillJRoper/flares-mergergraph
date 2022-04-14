01b87bc20ecb40d8415709d8b5ab76cf77c777dc
wjr21 <wjr21@sussex.ac.uk>
2022-04-14 21:24:52 +0100

    pivot to reading from raw data

--- FLARES_to_MEGA.py
+++ FLARES_to_MEGA.py
@@ -36,7 +36,7 @@ def get_data(tictoc, reg, tag):
 
     # Open single file and get DM particle mass
     hdf = h5py.File(single_file, "r")
-    part_dm_mass = hdf["Header"].attrs["MassTable"][1]
+    part_dm_mass = hdf["Header"].attrs["MassTable"][1] * 10 ** 10
     hdf.close()
 
     # Define the NULL value in GADGET files
