d6f5f9016cdcc35874e6f339b7105c8f82bb8334
wjr21 <wjr21@sussex.ac.uk>
2022-08-11 14:36:59 +0100

    Added passive galaxy plot for nprog

--- passive_props.py
+++ passive_props.py
@@ -6,6 +6,21 @@ import matplotlib.pyplot as plt
 from mega.core.talking_utils import pad_print_middle
 
 
+def print_info(grp, subgrp, mega_ind, true_nprog, nprog_major, prog_mass_cont,
+               prog_npart_cont):
+
+    pad = 15
+    header = "=" * pad + \
+        " LINKING DATA FOR GALAXY: (%d, %d = %d) " % (
+            grp, subgrp, mega_ind) + "=" * pad
+    length = len(header)
+    print(pad_print_middle("Nprog_all:", true_nprog, length=length))
+    print(pad_print_middle("Nprog_major:", nprog_major, length=length))
+    print(pad_print_middle("ProgMassContribution:", prog_mass_cont, length=length))
+    print(pad_print_middle("ProgNPartContribution:", prog_npart_cont, length=length))
+    print("=" * length)
+
+
 def get_galaxy_info():
 
     # Define paths
@@ -32,7 +47,6 @@ def get_galaxy_info():
     # Find this galaxy's MEGA ID
     grps = hdf["group_number"][...]
     subgrps = hdf["subgroup_number"][...]
-    print(grps, subgrps)
     mega_ind = np.where(np.logical_and(grps == grp, subgrps == subgrp))[0]
 
     print("GroupNumber:", grp, "SubGroupNumber:", subgrp,
@@ -67,15 +81,137 @@ def get_galaxy_info():
     hdf.close()
 
     # Print out this halos results from the graph
-    header = "=" * 7 + \
-        " LINKING DATA FOR GALAXY: (%d, %d) " % (grp, subgrp) + "=" * 7
-    length = len(header)
-    print(pad_print_middle("Nprog_all:", true_nprog, length=length))
-    print(pad_print_middle("Nprog_major:", nprog_major, length=length))
-    print(pad_print_middle("ProgMassContribution:", prog_mass_cont, length=length))
-    print(pad_print_middle("ProgNPartContribution:", prog_npart_cont, length=length))
-    print("=" * length)
+    print_info(grp, subgrp, mega_ind, true_nprog, nprog_major, prog_mass_cont,
+               prog_npart_cont)
+
+
+def plot_merger_ssfr():
+
+    # Define paths
+    path = "/cosma/home/dp004/dc-rope1/cosma7/FLARES/flares-mergergraph/"
+    halo_base = "data/halos/MEGAFLARES_halos_<reg>_<snap>.hdf5"
+    graph_base = "data/dgraph/MEGAFLARES_graph_<reg>_<snap>.hdf5"
+    qui_path = "/cosma7/data/dp004/dc-love2/codes/flares_passive/" + \
+        "analysis/data/select_quiescent.h5"
+    flares_path = "/cosma7/data/dp004/dc-payy1/my_files/flares_pipeline" + \
+        "/data/flares.hdf5"
+
+    # Open flares files
+    hdf_q = h5py.File(qui_path, "r")
+    hdf_f = h5py.File(flares_path, "r")
+
+    # What snapshot are we doing?
+    snap = "010_z005p000"
+
+    # Set up lists to collect our results
+    tot_nprogs = []
+    tot_ssfrs = []
+    central = []
+
+    # Loop over regions
+    for reg in hdf_f.keys():
+
+        print("Region", reg)
+
+        # Get FLARES galaxy IDs
+        grps = hdf_f[reg][snap]["Galaxy"]["GroupNumber"]
+        subgrps = hdf_f[reg][snap]["Galaxy"]["SubGroupNumber"]
+
+        # Get sSFRs
+        ssfrs = hdf_q[snap][reg]["sSFR"]["50Myr"][...]
+
+        # Open MEGA file
+        this_halo_base = halo_base.replace("<reg>", reg)
+        this_halo_base = halo_base.replace("<snap>", snap)
+        this_graph_base = graph_base.replace("<reg>", reg)
+        this_graph_base = graph_base.replace("<snap>", snap)
+        hdf_halo = h5py.File(this_halo_base, "r")
+        hdf_graph = h5py.File(this_graph_base, "r")
+
+        # Get MEGA galaxy IDs and masses
+        mega_grps = hdf_halo["group_number"][...]
+        mega_subgrps = hdf_halo["subgroup_number"][...]
+        masses = hdf_halo["masses"][...]
+        hdf_halo.close()
+
+        # Get contribution information
+        prog_mass_conts = hdf_graph["ProgMassContribution"][...]
+        prog_npart_conts = hdf_graph["ProgNPartContribution"][...]
+        start_index = hdf_graph["prog_start_index"][...]
+        nprog = hdf_graph["n_prog"][...]
+        hdf_graph.close()
+
+        # Loop over MEGA galaxies getting the corresponding FLARES galaxy
+        for ind in range(grps.size):
+
+            # Extract this group, subgroup and ssfrs
+            g, sg = grps[ind], subgrps[ind]
+            ssfr = ssfrs[ind]
+
+            # Which mega galaxy is this?
+            mega_ind = np.where(np.logical_and(mega_grps == g,
+                                               mega_subgrps == sg))[0]
+
+            # Get this galaxy's data
+            start = start_index[mega_ind]
+            stride = nprog[mega_ind]
+            prog_cont = prog_mass_conts[start: start + stride]
+            prog_ncont = prog_npart_conts[start: start + stride]
+            mass = masses[mega_ind]
+
+            # Limit galaxy's contribution to those contributing at least 10%
+            tot_prog_cont = np.sum(prog_cont, axis=1)
+            frac_prog_cont = tot_prog_cont / np.sum(mass)
+            okinds = frac_prog_cont > 0.1
+
+            # Get only "true" contributions
+            nprog = tot_prog_cont[okinds].size
+
+            if ssfr < 10**-1:
+                print_info(g, sg, mega_ind, stride, nprog,
+                           prog_cont[okinds, :], prog_ncont[okinds, :])
+
+            # Include this result
+            tot_nprogs.append(nprog)
+            tot_ssfrs.append(ssfr)
+            if sg == 0:
+                central.append(1)
+            else:
+                central.append(0)
+
+    # Convert to arrays
+    tot_nprogs = np.array(tot_nprogs, dtype=int)
+    tot_ssfrs = np.array(tot_ssfrs, dtype=np.float64)
+    central = np.array(central, dtype=bool)
+
+    # Set up plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # Plot the scatter of "normal" galaxies
+    ax.scatter(np.log10(tot_ssfrs), tot_nprogs,
+               marker=".", color="k", alpha=0.6)
+
+    # Plot the scatter for passive galaxies split by central status
+    okinds = tot_ssfrs < 10 ** -1
+    cent = central[okinds]
+    ax.scatter(np.log10(tot_ssfrs[okinds][cent]), tot_nprogs[okinds][cent],
+               marker="o", color="r", label="Central")
+    ax.scatter(np.log10(tot_ssfrs[okinds][~cent]), tot_nprogs[okinds][~cent],
+               marker="o", color="g", label="Satellite")
+
+    # Label axes
+    ax.set_xlabel("$\log_{10}(\mathrm{sSFR} / \mathrm={Gyr})$")
+    ax.set_ylabel("$N_{prog}$")
+
+    # Legend
+    ax.legend()
+
+    # Save figure
+    fig.savefig("passive_nprog.png", bbox_inches="tight", dpi=100)
 
 
 if len(sys.argv) > 1:
     get_galaxy_info()
+else:
+    plot_merger_ssfr()
