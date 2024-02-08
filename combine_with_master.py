caf4775bc9ed388dd7eded75d91899818bbcb377
wjr21 <wjr21@sussex.ac.uk>
2024-02-08 16:56:05 +0000

    No need to read odl master once copy is made

--- combine_with_master.py
+++ combine_with_master.py
@@ -88,89 +88,82 @@ flares_snaps = [
 ]
 
 
-with h5py.File(new_file, "r+") as hdf:
-    with h5py.File(master_file, "r") as hdf_master:
-        # Loop over the regions
-        for reg in regs:
-            # Loop over the snapshots
-            for snap in flares_snaps:
-                print(reg, snap)
-                # Get the galaxy group
-                gal_grp = hdf_master[f"{reg}/{snap}/Galaxy"]
+with h5py.File(new_file, "r+") as hdf_master:
+    # Loop over the regions
+    for reg in regs:
+        # Loop over the snapshots
+        for snap in flares_snaps:
+            print(reg, snap)
+            # Get the galaxy group
+            gal_grp = hdf_master[f"{reg}/{snap}/Galaxy"]
 
-                # Define the mergergraph file
-                mergergraph_file = (
-                    mega_path + f"GEAGLE_{reg}/SubMgraph_{snap}.hdf5"
+            # Define the mergergraph file
+            mergergraph_file = (
+                mega_path + f"GEAGLE_{reg}/SubMgraph_{snap}.hdf5"
+            )
+            # Open the mergergraph file
+            with h5py.File(mergergraph_file, "r") as hdf_mergergraph:
+                # Get the master group and subgroup IDs
+                master_grpIDs = gal_grp["GroupNumber"][:]
+                master_subgrpIDs = gal_grp["SubGroupNumber"][:]
+
+                # Get the mergergraph group and subgroup IDs
+                mergergraph_subfindIDs = hdf_mergergraph["SUBFIND_halo_IDs"][:]
+                mergergraph_grpIDs = []
+                mergergraph_subgrpIDs = []
+                for subfindID in mergergraph_subfindIDs:
+                    grpID, subgrpID = extract_group_subgroup_ids(subfindID)
+                    mergergraph_grpIDs.append(grpID)
+                    mergergraph_subgrpIDs.append(subgrpID)
+
+                # Get the progenitor and descendant pointers and numbers
+                prog_ptrs = hdf_mergergraph["Prog_Start_Index"][:]
+                n_progs = hdf_mergergraph["nProgs"][:]
+
+                # Make a nice look up table for the merger graph pointer and
+                # length
+                mergergraph_lookup = {}
+                for i, (grpID, subgrpID) in enumerate(
+                    zip(mergergraph_grpIDs, mergergraph_subgrpIDs)
+                ):
+                    mergergraph_lookup[(grpID, subgrpID)] = (
+                        prog_ptrs[i],
+                        n_progs[i],
+                    )
+
+                # Loop over galaxies in the master file getting the data
+                pointers = np.zeros(len(master_grpIDs), dtype=np.int32)
+                nprogs = np.zeros(len(master_grpIDs), dtype=np.int32)
+                prog_star_ms = []
+                for ind, (grp, subgrp) in enumerate(
+                    zip(master_grpIDs, master_subgrpIDs)
+                ):
+                    # Get the mergergraph pointer and length
+                    prog_start_index, nprog = mergergraph_lookup[(grp, subgrp)]
+
+                    # Get the progenitor masses
+                    prog_masses = hdf_mergergraph["prog_stellar_masses"][
+                        prog_start_index : prog_start_index + nprog
+                    ]
+
+                    # Perform a stellar mass cut
+                    prog_masses = prog_masses[prog_masses > 1e8]
+
+                    # Sort by stellar mass (they're sorted by DM mass in the
+                    # MEGA file)
+                    prog_masses = np.sort(prog_masses)[::-1]
+
+                    # Store this data
+                    nprogs[ind] = prog_masses.size
+                    pointers[ind] = len(prog_star_ms)
+                    prog_star_ms.extend(prog_masses)
+
+                # Add the data to the master file under a new group
+                gal_grp.create_group("MergerGraph")
+                gal_grp["MergerGraph"].create_dataset(
+                    "Prog_Start_Index", data=pointers
                 )
-                # Open the mergergraph file
-                with h5py.File(mergergraph_file, "r") as hdf_mergergraph:
-                    # Get the master group and subgroup IDs
-                    master_grpIDs = gal_grp["GroupNumber"][:]
-                    master_subgrpIDs = gal_grp["SubGroupNumber"][:]
-
-                    # Get the mergergraph group and subgroup IDs
-                    mergergraph_subfindIDs = hdf_mergergraph[
-                        "SUBFIND_halo_IDs"
-                    ][:]
-                    mergergraph_grpIDs = []
-                    mergergraph_subgrpIDs = []
-                    for subfindID in mergergraph_subfindIDs:
-                        grpID, subgrpID = extract_group_subgroup_ids(subfindID)
-                        mergergraph_grpIDs.append(grpID)
-                        mergergraph_subgrpIDs.append(subgrpID)
-
-                    # Get the progenitor and descendant pointers and numbers
-                    prog_ptrs = hdf_mergergraph["Prog_Start_Index"][:]
-                    n_progs = hdf_mergergraph["nProgs"][:]
-
-                    # Make a nice look up table for the merger graph pointer and
-                    # length
-                    mergergraph_lookup = {}
-                    for i, (grpID, subgrpID) in enumerate(
-                        zip(mergergraph_grpIDs, mergergraph_subgrpIDs)
-                    ):
-                        mergergraph_lookup[(grpID, subgrpID)] = (
-                            prog_ptrs[i],
-                            n_progs[i],
-                        )
-
-                    # Loop over galaxies in the master file getting the data
-                    pointers = np.zeros(len(master_grpIDs), dtype=np.int32)
-                    nprogs = np.zeros(len(master_grpIDs), dtype=np.int32)
-                    prog_star_ms = []
-                    for ind, (grp, subgrp) in enumerate(
-                        zip(master_grpIDs, master_subgrpIDs)
-                    ):
-                        # Get the mergergraph pointer and length
-                        prog_start_index, nprog = mergergraph_lookup[
-                            (grp, subgrp)
-                        ]
-
-                        # Get the progenitor masses
-                        prog_masses = hdf_mergergraph["prog_stellar_masses"][
-                            prog_start_index : prog_start_index + nprog
-                        ]
-
-                        # Perform a stellar mass cut
-                        prog_masses = prog_masses[prog_masses > 1e8]
-
-                        # Sort by stellar mass (they're sorted by DM mass in the
-                        # MEGA file)
-                        prog_masses = np.sort(prog_masses)[::-1]
-
-                        # Store this data
-                        nprogs[ind] = prog_masses.size
-                        pointers[ind] = len(prog_star_ms)
-                        prog_star_ms.extend(prog_masses)
-
-                    # Add the data to the master file under a new group
-                    hdf[f"{reg}/{snap}/Galaxy"].create_group("MergerGraph")
-                    hdf[f"{reg}/{snap}/Galaxy/MergerGraph"].create_dataset(
-                        "Prog_Start_Index", data=pointers
-                    )
-                    hdf[f"{reg}/{snap}/Galaxy/MergerGraph"].create_dataset(
-                        "nProgs", data=nprogs
-                    )
-                    hdf[f"{reg}/{snap}/Galaxy/MergerGraph"].create_dataset(
-                        "prog_stellar_masses", data=np.array(prog_star_ms)
-                    )
+                gal_grp["MergerGraph"].create_dataset("nProgs", data=nprogs)
+                gal_grp["MergerGraph"].create_dataset(
+                    "prog_stellar_masses", data=np.array(prog_star_ms)
+                )
